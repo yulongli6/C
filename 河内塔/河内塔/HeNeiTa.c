ab9b928bfca9a421a0c69942d1e34e57ed9e38fe
yulongli6 <18709202005@163.com>
2018-10-12 13:06:32 +0800

    2018.10.12
    添加计数功能

--- 河内塔/河内塔/HeNeiTa.c
+++ 河内塔/河内塔/HeNeiTa.c
@@ -2,25 +2,30 @@
 #include <stdio.h>
 #include <math.h>
 
-int Heneita(int n, char a, char b, char c)
+int Heneita(int n, char a, char b, char c,int *count)
 {
 	if (n == 1)
 	{
+		*count = 1;
 		printf("%c-->%c\n", a, c);
+		return 1;
 	}
-	else
-	{
-		Heneita(n - 1, a, c, b);
-		Heneita(1, a, b, c);
-		Heneita(n - 1, b, a, c);
-	}
+	int count1;
+	int count2;
+	int count3;
+	Heneita(n - 1, a, c, b, &count1);
+    Heneita(1, a, b, c, &count2);
+	Heneita(n - 1, b, a, c, &count3);
+	*count = count1 + count2 + count3;
+	return *count;
 }
 
 int main()
 {
 	int n = 0;
+	int count;
 	scanf("%d", &n);
-	Heneita(n, 'A', 'B', 'C');
-	printf("%d\n", (int)pow(2, n) - 1);
+	Heneita(n, 'A', 'B', 'C', &count);
+	printf("%d\n", count);
 	return 0;
 }
